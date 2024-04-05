6928e1c1f600034ac8365eb4df624ba11585baff
DemetrioPadre <148373150+DemetrioPadre@users.noreply.github.com>
2024-04-05 09:04:10 +0200

    Initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Project extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['title', 'content'];
+
+
+    public function getAbstract($n_chars = 30)
+    {
+        return (strlen($this->content) > $n_chars) ? substr($this->content, 0, $n_chars) . '...' : $this->content;
+    }
+}
---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-dark text-white">
+    <div class="container py-4">
+        &copy; {{ env('APP_AUTHOR', 'Demetrio Padre') }} {{ now()->year }}
+    </div>
+</footer>
