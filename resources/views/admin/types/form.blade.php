b21476c8e03207d79c61b37f8863c6ec56c0a7b5
DemetrioPadre <demetriopadre@gmail.com>
2024-04-08 19:57:17 +0200

    create create types

---
+++ resources/views/admin/types/form.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+@section('title', empty($type->id) ? 'Creazione Nuovoa Categoria' : 'Modifica Categoria')
+
+
+@section('content')
+    <section>
+        <div class="container ">
+            <a href="{{ route('admin.types.index') }}" class="my-4 btn btn-primary"><i
+                    class="fa-solid fa-circle-left fa-beat"></i>
+                Torna alla Lista Delle Categorie</a>
+
+
+            @if ($errors->any())
+                <div class="alert alert-danger ">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+
+            <h1 class="mb-4">{{ empty($type->id) ? 'Creazione Nuova Categoria' : 'Modifica Categoria' }}</h1>
+            <form action="{{ empty($type->id) ? route('admin.types.store') : route('admin.types.update', $type) }}"
+                class="row g-3" method="POST">
+                @if (!empty($type->id))
+                    @method('PATCH')
+                @endif
+
+                @csrf
+                <div class="col-12">
+                    <button class="btn btn-success"> <i class="fa-solid fa-floppy-disk me-1">
+                        </i>{{ empty($type->id) ? 'Salva' : 'Modifica' }}</button>
+                </div>
+            </form>
+
+        </div>
+    </section>
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -5,6 +5,9 @@
 
     <section>
         <div class="container ">
+            <a href="{{ route('admin.types.create') }}" class="my-4 btn btn-primary"><i class="fa-solid fa-plus me-2"></i>
+                Crea una Nuova
+                Categoria</a>
             <h1 class="mb-4">Lista categorie</h1>
             <table class="table mb-5">
                 <thead>
@@ -17,6 +20,17 @@
                 </thead>
                 <tbody>
                     @forelse ($types as $type)
+                        <tr>
+                            <td>{{ $type->id }}</td>
+                            <td>{{ $type->label }}</td>
+                            <td>{{ $type->color }}</td>
+                            <td>{!! $type->getBadge() !!}</td>
+                            <td></td>
+
+                        </tr>
+
+
+
                     @empty
                         <tr>
                             <td colspan="100%">
@@ -30,4 +44,9 @@
         </div>
     </section>
 
+@endsection
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endsection
